523610e7ca92ee4d59d4e8de60b5ddcd138ee233
tjdans345 <tjdans345@naver.com>
2021-04-26 08:16:33 +0900

    "코틀린, Activity 생명주기"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.finalpractice)
+        Log.d("life_cycle", "onCreate")
     }
-}
 
-fun test (): String {
+    override fun onStart() {
+        super.onStart()
+        Log.d("life_cycle", "onStart")
+    }
 
-    val s = "dasdsa"
-    return s
+    override fun onResume() {
+        super.onResume()
+        Log.d("life_cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("life_cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("life_cycle", "onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("life_cycle2", "onDestroy")
+
+    }
+
 }
