e049a996dbc47eda206609ed0aff1e0b18257e8f
tjdans345 <tjdans345@naver.com>
2021-04-27 16:48:50 +0900

    LayOut practice

--- app/src/main/java/com/example/myapplication/Listener.kt
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -2,8 +2,11 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.Kotlin.hello
+import com.example.myapplication.Kotlin.number
 import kotlinx.android.synthetic.main.activity_listener.*
 
 class Listener : AppCompatActivity() {
@@ -16,12 +19,35 @@ class Listener : AppCompatActivity() {
         // 1> 직접 찾아서 가져온다.
         // 2> xml을 import해서 가져온다.
         // hello
-        val textView : TextView = findViewById(R.id.hello3)
 
-        hello3.setOnClickListener {
-            Log.d("click2", "CLick!3!")
+        // 익명함수
+        // 1 -> 람다 방식
+//        hello3.setOnClickListener {
+//            Log.d("click" , "Click!!")
+//        }
+
+        // 2-> 익명 함수 방식
+//        hello3.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                Log.d("click", "Click")
+//            }
+//        })
+
+        //3 -> 이름이 필요한 경우(click)
+        val click = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                hello3.setText("안녕하세요")
+                image.setImageResource(R.drawable.bear)
+                number += 10
+                Log.d("number", "" + number)
+            }
         }
 
+        hello3.setOnClickListener(click)
+
+        // 뷰를 조작 하는 함수들
+        // 1> setText("안녕하세요")
+        // 2> setImageResource
 
     }
 
